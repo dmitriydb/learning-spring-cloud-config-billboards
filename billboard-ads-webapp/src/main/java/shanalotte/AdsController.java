36689bcf21541a8c11d320a06131a19f8b61771d
Dmitriy Drizhilo <dmitryd@logicon-group.ru>
2022-09-05 10:37:13 +0500

    Add ads service and tweaked config a little bit

---
+++ billboard-ads-webapp/src/main/java/shanalotte/AdsController.java
@@ -0,0 +1,36 @@
+package shanalotte;
+
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdsController {
+
+  @Value("${caps:false}")
+  private boolean isCaps;
+
+  @Value("${bold:false}")
+  private boolean isBold;
+
+  @Value("${italic:false}")
+  private boolean isItalic;
+
+  @GetMapping("/say")
+  public String showText() {
+    String text = "Buy cars! Call 440330 now";
+    if (isCaps) {
+      text = text.toUpperCase(Locale.ROOT);
+    }
+    if (isBold) {
+      text = String.format("<b>%s</b>", text);
+    }
+    if (isItalic) {
+      text = String.format("<b>%s</b>", text);
+    }
+    return text;
+  }
+}
---
+++ billboard-ads-webapp/src/main/java/shanalotte/AdsLauncher.java
@@ -0,0 +1,13 @@
+package shanalotte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsLauncher {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdsLauncher.class, args);
+  }
+
+}
