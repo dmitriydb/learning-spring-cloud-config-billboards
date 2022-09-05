ee9f4abe438077acaf79b2def54808650e52ceae
Dmitriy Drizhilo <dmitryd@logicon-group.ru>
2022-09-05 10:28:55 +0500

    Add taxi service integration with spring cloud config server

---
+++ billboard-taxi-webapp/src/main/java/ru/shanalotte/TaxiController.java
@@ -0,0 +1,34 @@
+package ru.shanalotte;
+
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaxiController {
+
+  @Value("${caps:false}")
+  private boolean isCaps;
+
+  @Value("${bold:false}")
+  private boolean isBold;
+
+  @Autowired
+  private Environment environment;
+
+  @GetMapping("/say")
+  public String showText() {
+    String text = "777030 Taxi Call now!";
+    if (isCaps) {
+      text = text.toUpperCase(Locale.ROOT);
+    }
+    if (isBold) {
+      text = String.format("<b>%s</b>", text);
+    }
+    return text;
+  }
+}
---
+++ billboard-taxi-webapp/src/main/java/ru/shanalotte/TaxiLauncher.java
@@ -0,0 +1,13 @@
+package ru.shanalotte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiLauncher {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaxiLauncher.class, args);
+  }
+
+}
