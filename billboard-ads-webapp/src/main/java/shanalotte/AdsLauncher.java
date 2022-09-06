ff4636a8a2ba9b27f85521ec8eb5b30ee3844e42
Dmitriy Drizhilo <drizhiloda@gmail.com>
2022-09-06 12:35:29 +0500

    Register both services as Eureka clients

--- billboard-ads-webapp/src/main/java/shanalotte/AdsLauncher.java
+++ billboard-ads-webapp/src/main/java/shanalotte/AdsLauncher.java
@@ -2,8 +2,10 @@ package shanalotte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdsLauncher {
 
   public static void main(String[] args) {
